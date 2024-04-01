a3c6d0dd84c7144b75d8522d463491894d2427b9
sardor <136901570+salimovone@users.noreply.github.com>
2024-04-02 04:37:40 +0500

    navbar created

---
+++ src/components/Navbar/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    transition: all 0.3s linear;
+    border-radius: 50%;
+    &:hover{
+        transform: rotate(360deg);
+        background-color: aqua;
+    }
+`
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Effect from '../../components/Effect'
+
+const Home = () => {
+  return (
+    <div>
+        <Effect />
+    </div>
+  )
+}
+
+export default Home
