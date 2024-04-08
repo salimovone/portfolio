c964f74bda35ccd49edcf615376e78a43833bd67
sardor <136901570+salimovone@users.noreply.github.com>
2024-04-08 17:36:25 +0500

    navbar updated

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,25 +2,21 @@ import { CgArrowTopRightO } from "react-icons/cg";
 import { AiOutlineMenu } from "react-icons/ai";
 import React from "react";
 import { Button, Li } from "./style";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const mobile = useSelector(state => state.navbar.mobile)
-
+  const navigate = useNavigate()
   return (
     <div className="w-full rounded-2xl border border-[var(--black-neutral3)] glass py-[10px] px-5 flex items-center justify-between">
-      {mobile ? (
-        <span className="text-white text-4xl active:text-[aqua]">
+        <span className="text-white text-4xl lg:hidden active:text-[aqua]">
           <AiOutlineMenu />
         </span>
-      ) : (
-        <Link to={"/"} className="font-bold text-[20px] text-white">
+        <Link to={"/"} className="font-bold text-[20px] max-lg:hidden text-white">
           Salimov<span className="text-[aqua]">ONE</span>
         </Link>
-      )}
-      {!mobile && (
-        <ul className="navLinks flex gap-2 text-white">
+
+
+        <ul className="navLinks flex gap-4 text-white max-lg:hidden">
           <Li>
             <Link to={"/"}>Home</Link>
           </Li>
@@ -34,10 +30,9 @@ const Navbar = () => {
             <Link to={"/contact"}>Contact</Link>
           </Li>
         </ul>
-      )}
 
       <div className="flex text-white gap-4 items-center justify-center">
-        <button className="active:text-[aqua]">Let's Talk</button>
+        <button className="active:text-[aqua]" onClick={()=>navigate("/contact")}>Let's Talk</button>
         <Button className="flex items-center text-3xl">
           <CgArrowTopRightO />
         </Button>
